83bc20817ec563219ad559f455585317dcd45450
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-03 14:35:01 +0530

    Time: 48 ms (0%), Space: 55.2 MB (6.8%) - LeetHub

--- 2491-smallest-even-multiple/2491-smallest-even-multiple.js
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.js
@@ -4,12 +4,15 @@
  */
 var smallestEvenMultiple = function(n) {
     let i=1;
+    let s=0;
     while(true){
         if(i%2==0&&i%n==0){
-            return i
+            s=i;
+            break;
         }
         else{
             i++;
         }
     }
+    return s
 };
