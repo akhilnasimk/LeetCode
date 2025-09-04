d4ef55e34b29ae75c52cf1d4be88ae2daddd63f3
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-04 14:06:32 +0530

    Time: 44 ms (40.74%), Space: 53.3 MB (76.54%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
 var numberOfMatches = function(n) {
-    return n-1  
+    if(n == 1) return 0
+    if(n % 2 == 0) 
+        return n/2 + numberOfMatches(n/2)
+    else 
+        return (n-1)/2 + numberOfMatches((n-1)/2+1) 
 };
