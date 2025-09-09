18e0ca61e4af30b866cdc18c5e3a4e4fdf9b7c16
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-09 11:30:48 +0530

    Time: 608 ms (5.1%), Space: 56.4 MB (94.51%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.js
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.js
@@ -4,14 +4,15 @@
  */
 var minOperations = function(boxes) {
     let res=[]
+    let sum=0;
     for(let j=0;j<boxes.length;j++){
-        let sum=0;
         for(let i=0;i<boxes.length;i++){
             if(boxes[i]==1){
                 sum+=Math.abs(i-j)
             }
         }
         res[j]=sum
+        sum=0;
     }
     return res
 };
