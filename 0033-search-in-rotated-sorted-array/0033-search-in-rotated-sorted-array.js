e6e5af3210e48cd8ac2f6ba7555d400da169c75d
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-13 20:54:22 +0530

    Time: 0 ms (100%), Space: 54.4 MB (21.74%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if(nums.indexOf(target)==0 || nums.indexOf(target)){
+        return nums.indexOf(target)
+    }
+    return -1
+};
