2eb016c3030863ce5eac7678b50041ed204ee14b
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-19 14:27:11 +0530

    Time: 0 ms (100%), Space: 55.8 MB (81.69%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if(n==1)return true 
+    if(n==0)return false 
+    if(n<0)return false
+    while(n>1){
+        if(n%2!==0)return false
+        n/=2;
+    }
+    return true
+};
