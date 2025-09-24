b9ff13bb5de1a449ea384337915be590da0e9f39
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-24 08:01:45 +0530

    Time: 0 ms (100%), Space: 5.3 MB (99.83%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.go
@@ -0,0 +1,18 @@
+import "strconv"
+func fizzBuzz(n int) []string {
+   res := make([]string ,0,n)
+//    res=res.append(res,"1")
+   fmt.Print(res)
+   for  i:=1 ;i<=n ;i++{
+    if(i%3 ==0 && i%5==0){
+        res=append(res,"FizzBuzz")
+    }else if(i%3==0){
+        res=append(res,"Fizz")
+    }else if(i%5==0){
+        res=append(res,"Buzz");
+    }else{
+        res=append(res,strconv.Itoa(i))
+    }
+   }
+   return res
+}
