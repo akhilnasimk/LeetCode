c281a103b6a887ca3bdc54cb14626f6b06e99a3a
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-25 15:36:58 +0530

    Time: 0 ms (100%), Space: 5 MB (57.21%) - LeetHub

---
+++ 0120-triangle/0120-triangle.go
@@ -0,0 +1,16 @@
+func minimumTotal(triangle [][]int) int {
+    n := len(triangle)
+    dp := make([]int, len(triangle[n-1]))
+copy(dp, triangle[n-1])
+
+for i := n-2; i >= 0; i-- {
+    for j := 0; j <= i; j++ {
+        if dp[j] < dp[j+1] {
+            dp[j] = triangle[i][j] + dp[j]
+        } else {
+            dp[j] = triangle[i][j] + dp[j+1]
+        }
+    }
+}
+return dp[0]
+}
