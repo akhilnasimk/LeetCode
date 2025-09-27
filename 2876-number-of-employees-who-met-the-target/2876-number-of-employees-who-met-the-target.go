9b73243dfd0e114d7abf482e9b1cfe830767c256
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-09-27 08:15:35 +0530

    Time: 0 ms (100%), Space: 4.5 MB (38.1%) - LeetHub

---
+++ 2876-number-of-employees-who-met-the-target/2876-number-of-employees-who-met-the-target.go
@@ -0,0 +1,9 @@
+func numberOfEmployeesWhoMetTarget(hours []int, target int) int {
+    count:=0
+    for _,val :=range hours{
+        if val>=target{
+            count++
+        }
+    }
+    return count
+}
