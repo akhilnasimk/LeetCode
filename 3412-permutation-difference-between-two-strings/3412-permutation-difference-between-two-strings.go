ff4f28af6152d19fb4e29daacc4109ebe76e71a5
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-10-03 11:50:40 +0530

    Time: 0 ms (100%), Space: 5.1 MB (11.63%) - LeetHub

--- 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.go
+++ 3412-permutation-difference-between-two-strings/3412-permutation-difference-between-two-strings.go
@@ -2,11 +2,9 @@ func findPermutationDifference(s string, t string) int {
     sMap:=make(map[string]int,len(s))
     tMap:=make(map[string]int,len(t))
     res:=0.0
-    for ind,val:=range s{
-        sMap[string(val)]=ind
-    }
-    for ind,val:=range t{
-        tMap[string(val)]=ind
+    for i :=range len(s){
+        sMap[string(s[i])]=i
+        tMap[string(t[i])]=i
     }
     for key,_ :=range tMap{
         res+=(math.Abs(float64(sMap[key]-tMap[key])))
