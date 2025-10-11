758fb42088747adffd0814e5ff0a3fe33b2252ec
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-10-11 20:01:28 +0530

    Time: 48 ms (5.95%), Space: 7.7 MB (20.03%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.go
+++ 0141-linked-list-cycle/0141-linked-list-cycle.go
@@ -1,27 +1,16 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func hasCycle(head *ListNode) bool {
-    cur:=head 
-    paths:=[]*ListNode{}
-    flag:=false 
-    outer:
-    for cur!=nil{
-        for _,val:=range paths {
-            if val==cur {
-                flag=true 
-                break outer 
+    cur := head
+    paths := []*ListNode{}
+
+    for cur != nil {
+        for _, node := range paths {
+            if node == cur {
+                return true // directly return
             }
         }
-        paths=append(paths,cur)
-        cur=cur.Next
+        paths = append(paths, cur)
+        cur = cur.Next
     }
-    if flag{
-        return true 
-    }
-    return false 
+
+    return false
 }
