32e7fb219c1ce5b775db42ac7f51e9c9990a79f7
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-10-18 22:59:37 +0530

    Time: 0 ms (100%), Space: 4.2 MB (45.31%) - LeetHub

---
+++ 3321-type-of-triangle/3321-type-of-triangle.go
@@ -0,0 +1,13 @@
+func triangleType(nums []int) string {
+    if  nums[0]+nums[1] > nums[2] && nums[0]+nums[2] > nums[1] && nums[1]+nums[2] > nums[0]{
+    if nums[0]==nums[1] && nums[1]==nums[2]{
+        return "equilateral" 
+    }else if nums[0]==nums[1] || nums[0]==nums[2] || nums[1]==nums[2]{
+        return "isosceles"
+    }
+    return "scalene" 
+    }
+    return "none"
+   
+    
+}
