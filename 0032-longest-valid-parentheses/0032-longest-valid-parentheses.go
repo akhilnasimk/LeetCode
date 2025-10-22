4e8d2234ff558416f9e0fb18ccfe4cd878aefeec
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-10-22 10:13:29 +0530

    Time: 3 ms (44.63%), Space: 8.2 MB (38.44%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.go
@@ -0,0 +1,22 @@
+func longestValidParentheses(s string) int {
+    stack := []int{-1}
+    maxLen := 0
+
+    for i, ch := range s {
+        if ch == '(' {
+            stack = append(stack, i)
+        } else {
+            stack = stack[:len(stack)-1]
+            if len(stack) == 0 {
+                stack = append(stack, i)
+            } else {
+                length := i - stack[len(stack)-1]
+                if length > maxLen {
+                    maxLen = length
+                }
+            }
+        }
+    }
+
+    return maxLen
+}
