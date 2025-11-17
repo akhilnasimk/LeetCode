391905249cc38a3efb63a755720e6a3cddfd44aa
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-11-17 14:24:00 +0530

    Time: 0 ms (100%), Space: 9.5 MB (66.67%) - LeetHub

--- 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.go
+++ 1548-check-if-all-1s-are-at-least-length-k-places-away/1548-check-if-all-1s-are-at-least-length-k-places-away.go
@@ -1,18 +1,12 @@
 func kLengthApart(nums []int, k int) bool {
-    count:=0
-    flag:=true 
-    for _,val:=range nums {
-        if val==0{
-            count++
-        }else{
-            fmt.Println(count)
-            if count<k && !flag{
-                
-                return false
-            }
-            count=0
-            flag=false 
-        }
-    }
-    return true 
+	last := -1
+	for i, val := range nums {
+		if val == 1 {
+			if (i-last)-1 < k && last != -1 {
+				return false
+			}
+			last = i
+		}
+	}
+	return true
 }
