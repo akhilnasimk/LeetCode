a1517b68986fd359910f9037e5ee1281a71e01e1
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-12-12 14:22:49 +0530

    Time: 68 ms (62.5%), Space: 16.1 MB (50%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.go
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.go
@@ -1,13 +1,7 @@
 func countDistinctIntegers(nums []int) int {
     unique:=make(map[int]bool)
-    count:=0
     for i:=range len(nums){
-        _,yee:=unique[nums[i]]
-        if !yee{
-            // fmt.Println("from array",nums[i])
-            unique[nums[i]]=true
-            count++
-        }
+        unique[nums[i]]=true
         temp:=nums[i]/10
         newnum:=nums[i]%10
         for temp>0{
@@ -15,12 +9,7 @@ func countDistinctIntegers(nums []int) int {
             newnum=(10*newnum)+rem
             temp=temp/10
         }
-        _,exist:=unique[newnum]
-        if !exist{
-            unique[newnum]=true
-            // fmt.Println(newnum)
-            count++
-        }
+        unique[newnum]=true
     }
-    return count
+    return len(unique)
 }
