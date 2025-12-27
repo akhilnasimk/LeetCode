cba652cf3bdc75e927bde98a3011813554799e64
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-12-27 22:09:43 +0530

    Time: 0 ms (100%), Space: 5.7 MB (21.24%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
@@ -10,17 +10,17 @@ func levelOrder(root *TreeNode) [][]int {
     dep:=Depth(root)
     res := make([][]int, dep)
 
-    Add(root,0,res)
+    Add(root,0,&res)
 
     return res
 
 }
 
-func Add(root *TreeNode ,index int , array [][]int){
+func Add(root *TreeNode ,index int , array *[][]int){
     if root==nil{
         return 
     }
-    array[index]=append(array[index],root.Val)
+    (*array)[index]=append((*array)[index],root.Val)
     Add(root.Left,index+1,array)
     Add(root.Right,index+1,array)
 }
