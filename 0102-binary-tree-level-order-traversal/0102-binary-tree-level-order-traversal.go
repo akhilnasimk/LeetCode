4258a6bc8ffb2b7e33c81d25f89c82455473d633
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-12-27 22:07:45 +0530

    Time: 0 ms (100%), Space: 5.7 MB (34.1%) - LeetHub

---
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.go
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func levelOrder(root *TreeNode) [][]int {
+    dep:=Depth(root)
+    res := make([][]int, dep)
+
+    Add(root,0,res)
+
+    return res
+
+}
+
+func Add(root *TreeNode ,index int , array [][]int){
+    if root==nil{
+        return 
+    }
+    array[index]=append(array[index],root.Val)
+    Add(root.Left,index+1,array)
+    Add(root.Right,index+1,array)
+}
+
+func Depth(root *TreeNode)int{
+    if root==nil{
+        return 0 
+    }
+
+    left:=Depth(root.Left)
+    right:=Depth(root.Right)
+
+    if left>right{
+        return left+1
+    }
+    return right+1
+}
