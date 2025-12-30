dc23c9ad8d1a5d3c6c6799758b17894beb35bbc6
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2025-12-30 20:39:03 +0530

    Time: 6 ms (80%), Space: 9.6 MB (75%) - LeetHub

---
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.go
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func getAllElements(root1 *TreeNode, root2 *TreeNode) []int {
+    ar1:=[]int{}
+    ar2:=[]int{}
+    var dfs func(*TreeNode ,*[]int )
+    dfs=func(r1 *TreeNode,arr *[]int ){
+        if r1==nil{
+            return 
+        }
+        
+        dfs(r1.Left,arr)
+        *arr=append(*arr,r1.Val)
+        dfs(r1.Right,arr)
+    }
+    dfs(root1,&ar1)
+    dfs(root2,&ar2)
+    res:=[]int{}
+    i:=0
+    j:=0
+    for i<len(ar1)&&j<len(ar2){
+        if ar1[i]<ar2[j]{
+            res=append(res,ar1[i])
+            i++
+        }else{
+            res=append(res,ar2[j])
+            j++
+        }
+    }
+    res=append(res,ar1[i:]...)
+    res=append(res,ar2[j:]...)
+    return res
+}
