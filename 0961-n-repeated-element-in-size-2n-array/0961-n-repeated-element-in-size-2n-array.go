af413511cfc7f8c9a1d33844c7277e1b1462181b
akhilnasimk <160366572+akhilnasimk@users.noreply.github.com>
2026-01-02 19:44:40 +0530

    Time: 5 ms (25%), Space: 8.9 MB (27.78%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.go
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.go
@@ -1,13 +1,11 @@
 func repeatedNTimes(nums []int) int {
     n:=len(nums)/2
     mapf:=make(map[int]int)
-    res:=0
     for _,val:=range nums{
         mapf[val]++
         if v,_:=mapf[val];v==n{
-            res=val
-            break 
+           return val 
         }
     }
-    return res 
+    return -1
 }
